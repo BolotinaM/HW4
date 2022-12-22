50fd43b430fcf86350df6646a0273f87fda4ef4b
Mariia <bolotina.23reg@yandex.ru>
2022-12-22 17:19:47 +0300

    Добавила решение задачи №2

--- HW4/2/Program.cs
+++ HW4/2/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int number = ReadInt("");
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+
+// Фунции задачи № 2
+
+    // Функция ввода данных: 6 строка
+    int ReadInt(string message)
+    {
+        Console.Write(message);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    // Функция оплеределения длины числа (символов): 7 строка
+    int NumberLen(int a)
+    {
+        int i = 0;
+        while (a > 0)
+        {
+            a /= 10;
+            i++;
+        }
+        return i;
+    }
+
+    // Функция вывода суммы цифр в числе: 8 строка
+    void SumNumbers(int n, int len)
+    {
+        int sum = 0;
+        for (int j = 1; j <= len; j++)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+        Console.WriteLine(sum);
+    }
